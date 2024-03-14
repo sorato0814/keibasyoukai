1f48e195ef62fb8902e0512a8f8fa1beb73dfad7
Sorato Nakazawa <sorato.music0814@gmail.com>
2024-03-14 21:17:54 +0900

    Webサービス　DBと接続

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,3 @@ Tag.create([
   { name: 'タグ5' }
 ])
 
-User.create!(username:  "管理者",
-             email: "admin@example.jp",
-             password:  "11111111",
-             password_confirmation: "11111111",
-             admin: true)
