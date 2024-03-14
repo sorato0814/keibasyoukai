b7a96f4ec5d17e4fc8c164dd06b1fe3fa897dd4b
Sorato Nakazawa <sorato.music0814@gmail.com>
2024-03-14 22:10:49 +0900

    umades

--- db/migrate/20240210111434_create_horses.rb
+++ db/migrate/20240210111434_create_horses.rb
@@ -2,7 +2,6 @@ class CreateHorses < ActiveRecord::Migration[6.1]
   def change
     create_table :horses do |t|
       t.string :name
-      t.integer :uma_id
       t.string :year
       t.string :image
       t.text :about_uma
